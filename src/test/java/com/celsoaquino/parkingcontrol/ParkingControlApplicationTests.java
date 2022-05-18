9b7b8487cd83ea7e25ccd2d9cf39c6aa559f44d6
Celso Aquino <celso.aquino@ymail.com>
2022-05-18 11:21:05 -0300

    first commit

---
+++ src/test/java/com/celsoaquino/parkingcontrol/ParkingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celsoaquino.parkingcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
